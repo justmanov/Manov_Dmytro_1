a78fe095d8447a2da14f898f19f91ca50662077b
Dmitry <deadxell13@gmail.com>
2022-06-27 02:34:45 +0300

    Add Lab1

---
+++ Lab1/Lab1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+using System.Text.Json;
+
+
+namespace Lab1 
+    { 
+       
+        class Program
+        {
+            static void Main( string[] args)
+            {  
+                Time time = new Time();
+                time.SetTime();
+                time.ShowTime();
+                time.ChangeHour();
+                time.ChangeMinute();
+                time.ChangeSecond();
+                time.ShowTime();
+                Time timejson = new Time()
+                {
+                    hour = "7",
+                    minute ="20",
+                    second = "00"
+                };
+                timejson.ShowTime();
+            string json = JsonConvert.SerializeObject(timejson, Formatting.Indented);
+
+            File.WriteAllText(@"C:\Users\Game\source\repos\Lab1\time.json", json);
+            Console.WriteLine("...Creating json file with this data:");
+            Console.WriteLine(json);
+            string jsonString = File.ReadAllText(@"C:\Users\Game\source\repos\Lab1\newObject.json");
+            Time time2 = JsonConvert.DeserializeObject<Time>(jsonString);
+            Console.WriteLine("-------------------------");
+            time2.ShowTime();
+
+
+        }
+
+    }
+
+    }
---
+++ Lab1/Lab1/Time.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    class Time
+    {
+        public string? hour;
+        public string? minute;
+        public string? second;
+
+        public void ShowTime()
+        {
+            Console.WriteLine($"Time is {hour}:" + $"{minute}:" + $"{second}");
+
+        }
+
+        public void SetTime()
+        {
+
+            Console.WriteLine("Enter Hour");
+            hour = Console.ReadLine();
+            int a = Convert.ToInt32(hour);
+            while (a < 0 || a > 24)
+            {
+                Console.WriteLine("Wrong Hour enter new one");
+                hour = Console.ReadLine();
+                a = Convert.ToInt32(hour);
+            }
+
+            Console.WriteLine("Enter Minute");
+            minute = Console.ReadLine();
+            int b = Convert.ToInt32(minute);
+            while (b < 0 || b > 60)
+            {
+                Console.WriteLine("Wrong Minute enter new one");
+                minute = Console.ReadLine();
+                b = Convert.ToInt32(minute);
+            }
+            Console.WriteLine("Enter Second");
+            second = Console.ReadLine();
+            int c = Convert.ToInt32(second);
+            while (c < 0 || c > 60)
+            {
+                Console.WriteLine("Wrong Second enter new one");
+                second = Console.ReadLine();
+                c = Convert.ToInt32(second);
+
+            }
+
+        }
+        public void ChangeHour()
+        {
+            Console.WriteLine("Enter new Hour");
+            hour = Console.ReadLine();
+            int a = Convert.ToInt32(hour);
+            while (a < 0 || a > 24)
+            {
+                Console.WriteLine("Wrong Hour enter new one");
+                hour = Console.ReadLine();
+                a = Convert.ToInt32(hour);
+            }
+        }
+        public void ChangeMinute()
+        {
+            Console.WriteLine("Enter new Minute");
+            minute = Console.ReadLine();
+            int b = Convert.ToInt32(minute);
+            while (b < 0 || b > 60)
+            {
+                Console.WriteLine("Wrong Minute enter new one");
+                minute = Console.ReadLine();
+                b = Convert.ToInt32(minute);
+            }
+        }
+        public void ChangeSecond()
+        {
+            Console.WriteLine("Enter new Second");
+            second = Console.ReadLine();
+            int c = Convert.ToInt32(second);
+            while (c < 0 || c > 60)
+            {
+                Console.WriteLine("Wrong Second enter new one");
+                second = Console.ReadLine();
+                c = Convert.ToInt32(second);
+
+            }
+        }
+    }
+}
